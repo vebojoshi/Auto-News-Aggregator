86f4d232fe136e7a8d7c9fcd08f7411feb403757
vebojoshi <vaibhav.vj81@gmail.com>
2021-06-04 03:08:54 +0530

    first commit

---
+++ news/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+import requests
+from bs4 import BeautifulSoup
+
+toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
+toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
+
+toi_headings = toi_soup.find_all('h2')
+
+toi_headings = toi_headings[0:-13] # removing footers
+
+toi_news = []
+
+for th in toi_headings:
+    toi_news.append(th.text)
+
+
+    def index(req):
+        return render(req, 'news/index.html', {'toi_news': toi_news})
+
