da19e8bc259f14c97148ea03a963907b0c1ec1d3
Vaibhav Joshi <vaibhav.vj81@gmail.com>
2021-06-04 09:29:30 +0530

    Update settings.py

--- AutoNewsAggregator/settings.py
+++ AutoNewsAggregator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r3$xtthr&e2r$+6g9l-d&m0nst9eu-phtwhzi%vvgu9-$s67lk'
+SECRET_KEY = 'KEEP IT SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
